559ed861651ba06036c10d39b7ac38e0ff79fa65
Olga Sozinova <olya.sozinova@api.ai>
2018-05-14 04:43:28 +0200

    comparative table changes

---
+++ edit_rhymings/edit.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import codecs
+import re
+
+class Editor():
+    f = codecs.open('hokkien.txt', 'r', 'utf-8')
+    search_all = re.compile(u'(.*?)\t(.*?)')
+    search_nums = re.compile(u'([0-9]+)-([0-9]+)')
+
+    def edit(self, line):
+        res = ''
+        nums = re.search(self.search_nums, line)
+        if nums is not None:
+            num1 = nums.group(1)
+            num2 = nums.group(2)
+            minus = int(num2) - int(num1)
+            if minus == 1:
+                res = u'paired\t' + num1 + '-' + num2
+            if minus == 2:
+                res = u'crossed\t' + num1 + '-' + num2
+            if minus == 3:
+                res = u'encircling\t' + num1 + '-' + num2
+            if minus > 3:
+                res = u'-\t' + num1 + '-' + num2
+        print res
+
+    def load(self):
+        for line in self.f:
+            self.edit(line)
+
+ed = Editor()
+ed.load()
